9b47fa658fedabcac54a224ae98cf4b2549b162a
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-08 19:08:18 -0500

    added a list of URLs and their shortened forms

--- express_server.js
+++ express_server.js
@@ -2,20 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+// {urls: {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// }}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls", (req,res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+// app.get("/urls/:shortURL", (req, res) => {
+//   // const templateVars = { shortURL: req.params.shortURL, longURL:};
+//   res.render("urls_show", templateVars);
+// });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+    const templateVars = { greeting: 'Hello World!'};
+    res.render("hello_world", templateVars);
+  });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
