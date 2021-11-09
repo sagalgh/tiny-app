4871035a603b5db124b3569cf06b9321d0b61d2b
Sagal Ghelle <sagal.ghelle@gmail.com>
2021-11-09 00:21:42 -0500

    added form and used POST request

--- express_server.js
+++ express_server.js
@@ -1,16 +1,25 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-// {urls: {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// }}
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+function generateRandomString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -19,18 +28,26 @@ app.get("/urls", (req,res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-// app.get("/urls/:shortURL", (req, res) => {
-//   // const templateVars = { shortURL: req.params.shortURL, longURL:};
-//   res.render("urls_show", templateVars);
-// });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
     const templateVars = { greeting: 'Hello World!'};
     res.render("hello_world", templateVars);
   });
-
+  
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send(generateRandomString(6));         
+  });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
